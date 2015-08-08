e8210e4c6158c04f2eadd009ab6d1946dca480e8
Cameron <cameron.p.buckingham@gmail.com>
2015-08-07 22:16:22 -0700

    still messing with button

--- js/states.js
+++ js/states.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
 
+  $("navbar-brand").click(function(){
+         $("p").hide();
+     });
 
-$('li').hover(
-function() {
-$(this)
-  .animate({'color': 'red'}, 400);
-},
-);
+  $('#li').hover(
+      function() {
+      	$('#li').css({'color' : 'red'});
+      },
+      function() {
+      	$('#li').css({'color' : 'blue'});
+      }
+  )
